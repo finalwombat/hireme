0d3dc16fe59ad115a7aeff5d09130c79cabaa2a2
Jay Kelly <comital333@gmail.com>
2018-02-20 07:27:00 +1000

    fix broken get request

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.send('Home server')
+  res.sendFile('./public/home.html')
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
