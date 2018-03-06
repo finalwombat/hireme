56e47c865c8b0d84b6daa702b0b296c99c9110fe
Jay Kelly <comital333@gmail.com>
2018-03-06 12:12:07 +1000

    fix res.sendfile

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express')
-
+const path = require('path')
 const app = express()
+
 const port = process.env.PORT || 5000
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.sendFile('./public/home.html')
+  res.sendFile(path.join(__dirname + '/public/home.html'))
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
